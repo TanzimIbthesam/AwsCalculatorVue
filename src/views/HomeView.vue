272743183f86bf98bd7a31ee631f3f24722fc917
Tanzim <tanzim67@gmail.com>
2022-04-21 18:57:09 +0600

    Unit multiplier fixed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,30 +4,17 @@
 
 
 
-  <div class="container mx-auto">
+  <div class="container mx-auto ">
     <p class="text-md text-gray-500 font-serif pt-">
       Select the units, number, and frequency for HTTP API requests based on expected volume. The calculations below exclude free tier discounts.</p>
       <p>HTTP API requests units</p>
       <p>The unit will apply to the number of requests you will specify below.</p>
-      {{selected}}
+      
       <select class="
-      mt-3
-      form-select form-select-lg mb-3
-      appearance-none
-      block
-      w-1/2
-      px-4
-      py-1
-      text-xl
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      selectoption
+      form-select form-select-lg 
+      mt-3 mb-3
+      focus:outline-non
       
       
       
@@ -38,30 +25,9 @@
         <option value=1000000>millions</option>
         
       </select>
-      <div class="flex">
-      <input type="number" class="px-6 py-1 h-8 border" v-model="requests" name="" id="">
-      <select class="
-      mt-3
-      form-select form-select-lg mb-3
-      appearance-none
-      block
-      w-1/2
-      px-4
-      py-1
-      text-xl
-      font-normal
-      text-gray-700
-      bg-white bg-clip-padding bg-no-repeat
-      border border-solid border-gray-300
-      rounded
-      transition
-      ease-in-out
-      m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      
-      
-      
-      " @change="switchSelectSecond($event)">
+      <div class="flex space-x-2 mt-3">
+      <input type="number" class="px-6 py-2 h-8 border mt-1 rounded-md" v-model="requests" name="" id="">
+      <select class="selectoption form-select form-select-lg focus:outline-none" @change="switchSelectSecond($event)">
         <option value="">Choose</option>
         <option value=2628000>per second</option>
         <option value=43800>per minute</option>
@@ -70,23 +36,44 @@
         <option value=1>per month</option>
       </select>
       </div>
-        SelectedSecond-{{totalRequestsperMinuteandaMonth}}  
-      SelectedSecond-{{selectedsecond}}  
-      <div v-if="selectedsecond>10000">a lott</div>
+      <div class="flex flex-col">
+        <p>Cache Memory</p>
+        <select class="selectoption form-select form-select-lg focus:outline-none" @change="switchcacheSelect($event)">
+        <option value="">Choose</option>
+        <option value=0.02>0.5</option>
+        <option value=0.038>1.6</option>
+        <option value=0.20>6.1</option>
+        <option value=0.25>13.5</option>
+        <option value=0.5>28.4</option>
+        <option value=1.0>58.2</option>
+        <option value=1.9>118</option>
+        <option value=3.8>237</option>
+      </select>
+      </div>
+        Total Requests -{{totalRequests}}  
+        
+      
       <br>
       <!-- 1 per second * (60 seconds in a minute x 60 minutes in an hour x 730 hours in a month)-{{requests*selected * 60*60*730}} -->
    
       Unit conversion for Api requests 
-       Requests: {{requests}} per second * (60 seconds in a minute x 60 minutes in an hour x 730 hours in a month)-{{totalRequestsperSecondnAMonth}}
-       <p class="text-xs">{{totalRequestsperSecondnAMonth}} requests * 1 unit multiplier={{totalRequestsperSecondnAMonth}}</p>
-       <p>Tiered price for: {{totalRequestsperSecondnAMonth}} requests </p>
-       <p>2628000 requests x 0.0000035000 USD ={{priceperMonthbasedonOneSecond}}USD</p>
+       Requests: {{requests}} per second * (60 seconds in a minute x 60 minutes in an hour x 730 hours in a month)-{{totalRequests}}
+       <p class="text-xs">{{totalRequests}} requests * {{selected}} unit multiplier={{totalRequests}}</p>
+       <p>Tiered price for: {{totalRequests}} requests </p>
+       <p>2628000 requests x 0.0000035000 USD ={{totalPricebasedonRequests}}USD</p>
+       <div v-if="selectedcache">
+           <p>{{selectedcache}} USD per hour * 730 hours in a month={{totalCacheCost}} USD for cache memory</p>
+       <p>Dedicated Cache memory total price-{{totalCacheCost}}</p>
+        <p>Total Price with Cache-{{totalPricewithChache}}</p>
+       </div>
+      
     </div>
 </template>
 <script setup>
 import {ref,computed} from 'vue'
 const selected=ref(1);
 const selectedsecond=ref('');
+const selectedcache=ref('');
 let requests=ref('');
 let age=ref(17);
 const switchSelect=(event)=>{
@@ -102,13 +89,23 @@ const switchSelectSecond=(event)=>{
   console.log(selectedsecond);
   
 }
-const totalRequestsperSecondnAMonth= computed (()=>{
-  return requests.value * selected.value * 60 * 60 * 730
+const switchcacheSelect=(event)=>[
+  selectedcache.value=event.target.value
+]
+// const totalRequestsperSecondnAMonth= computed (()=>{
+//   return requests.value * selected.value * 60 * 60 * 730
+// })
+ 
+ const totalRequests= computed (()=>{
+  return requests.value *selected.value* selectedsecond.value 
 })
- const priceperMonthbasedonOneSecond=computed(()=>{
-   return  requests.value * selected.value * 60 * 60 * 730 * 0.0000035
+const totalPricebasedonRequests=computed(()=>{
+   return  requests.value *selected.value* selectedsecond.value  * 0.0000035
  })
- const totalRequestsperMinuteandaMonth= computed (()=>{
-  return requests.value * selectedsecond.value 
+const totalCacheCost=computed(()=>{
+   return selectedcache.value * 730
+})
+const totalPricewithChache=computed(()=>{
+  return requests.value *selected.value* selectedsecond.value  * 0.0000035 + selectedcache.value * 730
 })
 </script>
